f318b387cc577bf2b1581290d43c143791c1bec4
Nicolas Osborne <nicolas.osborne@etudiant.univ-lille1.fr>
2018-12-11 17:46:59 +0100

    refactor(untyped): cleanup indentation and some notations

--- coq/untyped.v
+++ coq/untyped.v
@@ -1,63 +1,62 @@
 Module Untyped.
 
 
-  Section Utils.
+  (** Type declaration for the variables.  
+  According to the syntax of lambda calculus, we need an infinite
+  set of variables. *)
+  Inductive lvar : Type :=
+  | lvarO : lvar
+  | S : lvar -> lvar.
 
-    (** Type declaration for the variables.
+  Fixpoint lvar_beq (x: lvar) (y: lvar) : bool :=
+    match x with
+    | lvarO => match y with
+              | lvarO => true
+              | S y' => false
+              end
+    | S x' => match y with
+             | lvarO => false
+             | S y' => lvar_beq x' y'
+             end
+    end.
 
-    According to the syntax of lambda calculus, we need an infinite
-    set of variables. *)
-    Inductive lvar : Type :=
-    | lvarO : lvar
-    | S : lvar -> lvar.
+  (** Type declaration for the sets of variable.
+  Variables are strings, mainly in order to have an infinite
+  set of available variables according to the definition of
+  the syntax of lambda calculus.
+   *)
+  Inductive varset : Type := 
+  | nil : varset
+  | cons : lvar -> varset -> varset.
 
-    Fixpoint lvar_beq (x: lvar) (y: lvar) : bool :=
-      match x with
-      | lvarO => match y with
-                | lvarO => true
-                | S y' => false
-                end
-      | S x' => match y with
-               | lvarO => false
-               | S y' => lvar_beq x' y'
-               end
-      end.
-    
-    (** Type declaration for the sets of variable.
-     
-     Variables are strings, mainly in order to have an infinite
-     set of available variables according to the definition of
-     the syntax of lambda calculus.
-     
-     *)
-    Inductive varset : Type := 
-    | nil : varset
-    | cons : lvar -> varset -> varset.
+  Notation "x :: l" := (cons x l)
+                         (at level 60, right associativity).
+  Notation "[ ]" := nil.
+  Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..).
 
-    Notation "x :: l" := (cons x l)
-                     (at level 60, right associativity).
-    Notation "[ ]" := nil.
-    Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..).
+  Fixpoint addvar  (el : lvar) (s : varset) : varset :=
+    match s with
+    | nil => el :: nil
+    | h::t => if lvar_beq h el then t else h::(addvar el t)
+    end.
 
-    Fixpoint addvar  (el : lvar) (s : varset) : varset :=
-      match s with
-      | nil => el :: nil
-      | h::t => if lvar_beq h el then t else h::(addvar el t)
-      end.
+  Fixpoint union (s1: varset) (s2: varset) : varset :=
+    match s2 with
+    | nil => s1
+    | h::t => union (h::s1) t
+    end.
 
-    Fixpoint concat (s1: varset) (s2: varset) : varset :=
-      match s2 with
-      | nil => s1
-      | h::t => concat (h::s1) t
-      end.
-    
-    Fixpoint removevar (el: lvar) (s: varset) : varset :=
-      match s with
-      | nil => nil
-      | h::t => if lvar_beq el h then t else h::(removevar el t)
-      end.
-    
-  End Utils.
+  Notation "s1 ++ s2" := (union s1 s2).
+  
+  Fixpoint removevar (el: lvar) (s: varset) : varset :=
+    match s with
+    | nil => nil
+    | h::t => if lvar_beq el h then t else h::(removevar el t)
+    end.
+
+  Notation "x \ s" := (removevar x s)
+                        (at level 60, right associativity).
+
   
   (** Type declaration
       An untyped lambda term is either:
@@ -87,9 +86,9 @@ Module Untyped.
    *)
   Fixpoint freevar (t : Term) : varset :=
     match t with
-    | (var x) => cons x nil
-    | (app t1 t2) => concat (freevar t1) (freevar t2)
-    | (abs x t') => removevar x (freevar t')
+    | (var x) => x :: nil
+    | (app t1 t2) => (freevar t1) ++ (freevar t2)
+    | (abs x t') => x \ (freevar t')
     end.
 
 
