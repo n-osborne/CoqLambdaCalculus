7204efae6d9f01f492a80fd4a9282ba16cfd6214
Nicolas Osborne <nicolas.osborne@etudiant.univ-lille1.fr>
2018-12-14 21:00:23 +0100

    proof(untyped): equality is decidable for lvar
    Proof is still dirty.

--- coq/untyped.v
+++ coq/untyped.v
@@ -20,6 +20,36 @@ Module Untyped.
              end
     end.
 
+  Lemma eq_SxSy_implies_eq_xy_lvar : forall x y: lvar, S x = S y -> x = y.
+  Proof.
+    induction x.
+    - induction y.
+      + intro; reflexivity.
+      + intro H. inversion H.
+    - induction y.
+      + intro H. inversion H.
+      + intro H. inversion H. reflexivity.
+  Qed.
+
+  Lemma eq_xy_implies_eqSxSy_lvar : forall x y: lvar, x = y -> S x = S y.
+    induction x; induction y.
+    - intro; reflexivity.
+    - intro H; inversion H.
+    - intro H; inversion H.
+    - intro H; inversion H; reflexivity.
+  Qed.
+
+  Lemma eq_dec_lvar : forall x y : lvar, {x = y} + {x <> y}.
+  Proof.
+    induction x; destruct y.
+    - left; reflexivity.
+    - right; unfold not; intro H; inversion H.
+    - right; unfold not; intro H; inversion H.
+    - destruct (IHx y); [left|right].
+      + apply eq_xy_implies_eqSxSy_lvar. apply e.
+      + unfold not; intro H. inversion H. auto.
+  Qed.
+      
   (** Type declaration for the sets of variable.
   Variables are strings, mainly in order to have an infinite
   set of available variables according to the definition of
@@ -98,6 +128,19 @@ Module Untyped.
     | (abs x t') => x \ (freevar t')
     end.
 
+  Example freevar1 : forall x: lvar, freevar (var x) = [x].
+  intro x. reflexivity. Qed.
+
+  Example freevar2 : forall x y : lvar, freevar (app (var x) (var y)) = x::y::nil
+  \/ freevar (app (var x) (var y)) = y::x::nil.
+  Proof. intros. simpl. right. reflexivity. Qed.
+
+  Example freevar3 : forall x y : lvar, x <> y -> freevar (abs x (var y)) = [y].
+  Proof. intros. unfold not in H. simpl. destruct (lvar_beq x y).
+         - admit.
+         - reflexivity.
+  Qed.
+  
   (** Predicate to determine whether a term is a redex.
   * All the work is done when the argument is an application
   *)
@@ -111,6 +154,12 @@ Module Untyped.
                      | (abs x' b') => orb (invarset x' (freevar b')) (isredex t2)
                     end
     end.
+
+  Example isredex1 : forall x: lvar, forall y : lvar, is_true (isredex (app (abs x (var x)) (var y))).
+  Proof.
+
+
+
 
   (** Inductive Proposition about term **)
 
